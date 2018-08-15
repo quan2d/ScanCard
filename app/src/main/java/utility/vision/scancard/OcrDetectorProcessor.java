71caf1e7e616e648870920cd16a8742c9b950b91
Do Dang Quan <quandd@gmail.com>
2018-08-15 08:52:47 +0700

    init

---
+++ app/src/main/java/utility/vision/scancard/OcrDetectorProcessor.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package utility.vision.scancard;
+
+import android.util.Log;
+import android.util.SparseArray;
+
+import utility.vision.scancard.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+/**
+ * A very simple Processor which receives detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ */
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> mGraphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        mGraphicOverlay = ocrGraphicOverlay;
+    }
+
+    /**
+     * Called by the detector to deliver detection results.
+     * If your application called for it, this could be a place to check for
+     * equivalent detections by tracking TextBlocks that are similar in location and content from
+     * previous frames, or reduce noise by eliminating TextBlocks that have not persisted through
+     * multiple detections.
+     */
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        mGraphicOverlay.clear();
+        String data = "";
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            data = isCodeNumber(item.getValue());
+            if(data.length() > 0) {
+                OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+                mGraphicOverlay.add(graphic);
+                Log.d("receiveDetections", "Text read: " + item.getValue());
+            }
+        }
+    }
+
+    private String isCodeNumber(String text){
+        String rc = "";
+
+        if(text.length() >= 10 && text.length() <= 30){
+            rc = text.replaceAll("[^0-9]", "");
+            Log.d("isCodeNumber", "Text read: " + rc);
+            if(rc.length() < 10){
+                rc = "";
+            }
+        }else{
+            rc = "";
+        }
+
+        return rc;
+    }
+
+    /**
+     * Frees the resources associated with this detection processor.
+     */
+    @Override
+    public void release() {
+        mGraphicOverlay.clear();
+    }
+}
